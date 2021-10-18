93284681eb2f8029d1116bc099fe70806370abfc
kellar1995 <kellar_a@yahoo.com>
2021-10-18 16:11:54 -0500

    added products

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app, db
 from flask import render_template, redirect, url_for
 from app.forms import RegistrationForm
-from app.models import User
+from app.models import Product, User
 
 @app.route('/')
 def index():
@@ -19,5 +19,15 @@ def register():
         db.session.commit()
 
         return redirect(url_for('index'))
-        
+
     return render_template('register.html', form = register_form)
+
+@app.route('/products')
+def products():
+    all_products = Product.query.all()
+    return render_template('products.html', products=all_products)
+
+@app.route('/products/<prod_id>')
+def product_detail(prod_id):
+    product = Product.query.get_or_404(prod_id)
+    return render_template('product_detail.html', product=product)
